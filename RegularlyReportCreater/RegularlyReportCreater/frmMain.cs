9794ad2068ad38d0dee1553d1a7cda4e7f444069
kinoshita kazuma <vixen6630@hotmail.co.jp>
2017-02-11 12:28:29 +0900

    ソリューション作成

---
+++ RegularlyReportCreater/RegularlyReportCreater/frmMain.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace RegularlyReportCreater
+{
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// フォームロードイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            using (var connection = new SQLiteConnection("DATA Source=test.db"))
+            {
+                connection.Open();
+                using (SQLiteCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "create table hoge(id INTEGER PRIMARY KEY AUTOINCREMENT, field1 TEXT, field2 REAL, field3 BLOB)";
+                    command.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+        }
+    }
+
+
+
+
+
+
+}
