168ebd70141a2961fe48675062d0306d9629cb45
kinoshita kazuma <vixen6630@hotmail.co.jp>
2017-02-11 17:52:47 +0900

    メイン画面作成

--- RegularlyReportCreater/RegularlyReportCreater/frmMain.Designer.cs
+++ RegularlyReportCreater/RegularlyReportCreater/frmMain.Designer.cs
@@ -28,21 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnMasterEdit = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("HG創英角ﾎﾟｯﾌﾟ体", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(34, 24);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(236, 27);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "定期報告作るくん";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(39, 76);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(231, 50);
+            this.btnCreate.TabIndex = 1;
+            this.btnCreate.Text = "定期報告作成";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            // 
+            // btnMasterEdit
+            // 
+            this.btnMasterEdit.Location = new System.Drawing.Point(39, 178);
+            this.btnMasterEdit.Name = "btnMasterEdit";
+            this.btnMasterEdit.Size = new System.Drawing.Size(231, 50);
+            this.btnMasterEdit.TabIndex = 2;
+            this.btnMasterEdit.Text = "マスタ編集";
+            this.btnMasterEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(39, 286);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(231, 50);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "終了";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(301, 387);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnMasterEdit);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(321, 430);
+            this.MinimumSize = new System.Drawing.Size(321, 430);
             this.Name = "frmMain";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "定期報告作るくん";
             this.Load += new System.EventHandler(this.frmMain_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnMasterEdit;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- RegularlyReportCreater/RegularlyReportCreater/frmMain.cs
+++ RegularlyReportCreater/RegularlyReportCreater/frmMain.cs
@@ -1,50 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Data.SQLite;
+using System.Windows.Forms;
 
-namespace RegularlyReportCreater
-{
+namespace RegularlyReportCreater {
 
     /// <summary>
     /// コンストラクタ
     /// </summary>
-    public partial class frmMain : Form
-    {
-        public frmMain()
-        {
+    public partial class frmMain : Form {
+
+        public frmMain() {
             InitializeComponent();
         }
 
+        #region "イベント"
+
         /// <summary>
-        /// フォームロードイベント
+        /// フォームロードイベント。
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void frmMain_Load(object sender, EventArgs e)
-        {
-            using (var connection = new SQLiteConnection("DATA Source=test.db"))
-            {
-                connection.Open();
-                using (SQLiteCommand command = connection.CreateCommand())
-                {
-                    command.CommandText = "create table hoge(id INTEGER PRIMARY KEY AUTOINCREMENT, field1 TEXT, field2 REAL, field3 BLOB)";
-                    command.ExecuteNonQuery();
-                }
-                connection.Close();
-            }
+        private void frmMain_Load(object sender, EventArgs e) {
+            //using (var connection = new SQLiteConnection("DATA Source=test.db"))
+            //{
+            //    connection.Open();
+            //    using (SQLiteCommand command = connection.CreateCommand())
+            //    {
+            //        command.CommandText = "create table hoge(id INTEGER PRIMARY KEY AUTOINCREMENT, field1 TEXT, field2 REAL, field3 BLOB)";
+            //        command.ExecuteNonQuery();
+            //    }
+            //    connection.Close();
+            //}
         }
+
+        /// <summary>
+        /// 終了ボタンクリックイベント。
+        /// アプリケーションを終了します。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnExit_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        #endregion "イベント"
     }
-
-
-
-
-
-
 }
